6d2dfe48fddb5f3591cc3c50672e37f5a9e672e0
wonjin <wonjin2168@gmail.com>
2022-08-18 00:00:25 +0900

    #GalleryEx [Ing]
    - Glide 라이브러리 추가
    - Coroutine 라이브러리 추가
    - 갤러리 완료
    - 디자인 수정 작업

--- GalleryEx/app/src/main/java/com/jdroid/galleryex/GalleryActivity.kt
+++ GalleryEx/app/src/main/java/com/jdroid/galleryex/GalleryActivity.kt
@@ -3,46 +3,61 @@ package com.jdroid.galleryex
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.jdroid.galleryex.adapter.ImageAdapter
 import com.jdroid.galleryex.databinding.ActivityGalleryBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class GalleryActivity: AppCompatActivity() {
+class GalleryActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityGalleryBinding.inflate(layoutInflater) }
-    private val imageUriList = ArrayList<Uri>()
+    private val imageList = ArrayList<Uri>()
+    private var adapter: ImageAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        initData()
         initLayout()
+        initData()
         initListener()
     }
 
     private fun initData() {
-        val cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, arrayOf(MediaStore.Images.Media._ID), null, null, MediaStore.Images.ImageColumns.DATE_ADDED + " DESC")
+        CoroutineScope(Dispatchers.Main).launch {
+            withContext(Dispatchers.IO) {
+                val cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, arrayOf(MediaStore.Images.Media._ID), null, null, MediaStore.Images.ImageColumns.DATE_ADDED + " DESC")
 
-        cursor?.let {
-            val columnsIndex = it.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
-            while(it.moveToNext()) {
-                val imageId = it.getLong(columnsIndex)
-                imageUriList.add(Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "$imageId"))
-                Log.d("imageUri", "$imageId")
+                cursor?.let {
+                    val columnsIndex = it.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+                    while (it.moveToNext()) {
+                        val imageId = it.getLong(columnsIndex)
+                        val imageUri = Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "$imageId")
+                        imageList.add(imageUri)
+                    }
+                }
+                cursor?.close()
             }
+
+            setImage()
         }
-        cursor?.close()
+
     }
 
     private fun initLayout() {
         setContentView(binding.root)
 
-        Log.d("imageUriList", "$imageUriList")
-        binding.imageList.adapter = ImageAdapter(imageUriList)
+        adapter = ImageAdapter(imageList)
+        binding.imageList.adapter = adapter
     }
 
     private fun initListener() {
 
+    }
+
+    private fun setImage() {
+        adapter?.notifyItemRangeChanged(0, imageList.size)
     }
 }
--- GalleryEx/app/src/main/java/com/jdroid/galleryex/adapter/ImageAdapter.kt
+++ GalleryEx/app/src/main/java/com/jdroid/galleryex/adapter/ImageAdapter.kt
@@ -1,24 +1,44 @@
 package com.jdroid.galleryex.adapter
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.jdroid.galleryex.databinding.ViewImageItemsBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.Math.round
+import kotlin.math.roundToInt
 
-class ImageAdapter(private val uriList: ArrayList<Uri>): RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
+class ImageAdapter(private val uriList: ArrayList<Uri>) : RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(ViewImageItemsBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(ViewImageItemsBinding.inflate(LayoutInflater.from(parent.context), parent, false), parent.measuredWidth)
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind()
+        holder.bind(uriList[position])
     }
 
     override fun getItemCount() = uriList.size
 
-    inner class ViewHolder(private val binding: ViewImageItemsBinding): RecyclerView.ViewHolder(binding.root) {
-        fun bind() {
-            binding.imageView.setImageURI(uriList[adapterPosition])
+    inner class ViewHolder(private val binding: ViewImageItemsBinding, private val parentWidth: Int) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(uri: Uri) {
+            binding.layout.layoutParams = (binding.layout.layoutParams as ViewGroup.LayoutParams).apply {
+                height = parentWidth / 3
+            }
+            CoroutineScope(Dispatchers.Main).launch {
+                Glide.with(itemView.context)
+                    .load(uri)
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                    .thumbnail(Glide.with(itemView.context).asDrawable().load(uri).sizeMultiplier(0.05f))
+                    .into(binding.imageView)
+            }
         }
     }
 }
